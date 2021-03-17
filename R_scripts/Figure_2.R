83b3e58d8e887de15bb2aa1bb71039fa52a7339f
freidavid <78372940+freidavid@users.noreply.github.com>
2021-03-17 10:56:52 +0100

    Update Figure_2.R

--- R_scripts/Figure_2.R
+++ R_scripts/Figure_2.R
@@ -25,24 +25,13 @@ abline(h=0,lty=2)
 #define colors
 col=c("black","black","black","darkgrey","black","black","black","darkgrey","darkgrey")
 #draw squares
-points(y=data,x=number,col=col,pch=15)
+points(y=data,x=number,col=col,pch=3,lwd=2,cex=1.5)
 #text above different categories
 mtext(side=3,line=1,text="1 deeper into shallower",at=2)
 mtext(side=3,line=1,text="2 shallower into deeper",at=7)
 mtext(side=3,line=1,text="3 benthic into pelagic",at=12)
 mtext(side=3,line=1,text="4 pelagic into benthic",at=17.5)
 box()
-#error bars
-arrows(x0=number[1], y0=data[1]-0.0014, x1=number[1], y1=data[1]+0.0014, code=3, angle=90, length=0.1,lwd=2)
-arrows(x0=number[2], y0=data[2]-0.0014, x1=number[2], y1=data[2]+0.0014, code=3, angle=90, length=0.1,lwd=2)
-arrows(x0=number[3], y0=data[3]-0.0014, x1=number[3], y1=data[3]+0.0014, code=3, angle=90, length=0.1,lwd=2)
-arrows(x0=number[4], y0=data[4]-0.0014, x1=number[4], y1=data[4]+0.0014, code=3, angle=90, length=0.1,col="darkgrey",lwd=2)
-arrows(x0=number[5], y0=data[5]-0.0014, x1=number[5], y1=data[5]+0.0014, code=3, angle=90, length=0.1,lwd=2)
-arrows(x0=number[6], y0=data[6]-0.0014, x1=number[6], y1=data[6]+0.0014, code=3, angle=90, length=0.1,lwd=2)
-arrows(x0=number[7], y0=data[7]-0.0014, x1=number[7], y1=data[7]+0.0014, code=3, angle=90, length=0.1,lwd=2)
-arrows(x0=number[8], y0=data[8]-0.0014, x1=number[8], y1=data[8]+0.0014, code=3, angle=90, length=0.1,col="darkgrey",lwd=2)
-arrows(x0=number[9], y0=data[9]-0.0014, x1=number[9], y1=data[9]+0.0014, code=3, angle=90, length=0.1,col="darkgrey",lwd=2)
-
 
 dev.off()
 
